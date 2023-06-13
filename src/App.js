087ddfbe9b615991ec63a516b6d8f3a5bf14474a
LODJEN2002 <decel1337@gmail.com>
2023-06-13 21:28:13 +0500

    new colors and logo

--- src/App.js
+++ src/App.js
@@ -27,15 +27,47 @@ function App() {
   return (
     <main className="App">
       <div>
-        <h1>
-          Todo list
-        </h1>
+        <div className='ToDo__title'>
+          <div className='asd'>
+            <div>
+              <div>
+                T
+              </div>
+              <div>
+                D
+              </div>
+            </div>
+            <div>
+              <div className='Todo__totle_o'>
+                o
+              </div>
+              <div className='Todo__totle_o'>
+                o
+              </div>
+            </div>
+          </div>
+          <div className='Todo__totle_list'>
+            List
+          </div>
+          {/* <div className='ToDo__title-t'>
+            T
+            <div className='ToDo__title-d'>
+              D
+            </div>
+          </div>
+          <div className='ToDo__title-t'>
+            o
+            <div className='ToDo__title-o2'>
+              o
+            </div>
+          </div> */}
+        </div>
         <input
           value={text}
           placeholder='Введите текст'
           onChange={onChange}
           onKeyDown={(e) => {
-            if (e.code === 'Enter') {
+            if (e.key === 'Enter') {
               handleSubmit(e)
             }
           }}
